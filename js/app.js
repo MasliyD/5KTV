fde15862bfceaab432eef04ef17b23fdc27b67d9
MasliyD <masliyd93@gmail.com>
2022-08-15 19:59:10 +0300

    header - done, start adaptive

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        //slidesToShow: 3,
+        speed: 300, //500
+        easing: 'ease',
+        //infinite: false, //del
+        //waitForAnimate: false,
+        variableWidth: true
+    });
+});
