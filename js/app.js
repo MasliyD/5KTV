c9edc4b88a93a70515912d21083f2cb236604284
MasliyD <masliyd93@gmail.com>
2022-09-03 17:04:52 +0300

    with hover

--- js/app.js
+++ js/app.js
@@ -1,23 +1,17 @@
 $(document).ready(function(){
     $('.slider').slick({
-        // speed: 300,
-        // easing: 'ease',
-        // variableWidth: true
         infinite: true,
         speed: 300,
         slidesToShow: 5,
         slidesToScroll: 4,
-        //centerMode: true,
         variableWidth: true
     });
 
     $('.intro__slider').slick({
-        speed: 1200, 
+        speed: 800, 
         easing: 'ease',
-        //infinite: false,
         //waitForAnimate: false,
-        //variableWidth: true,
-        //autoplay: true,
+        autoplay: true,
         autoplaySpeed: 3500
     });
 
@@ -26,8 +20,7 @@ $(document).ready(function(){
         slidesToShow: 4,
         slidesToScroll: 2,
         //autoplay: true,
-        autoplaySpeed: 6000,
-        //waitForAnimate: false,
+        //autoplaySpeed: 6000,
         responsive: [
             {
                 breakpoint: 960,
@@ -53,8 +46,7 @@ $(document).ready(function(){
         slidesToShow: 4,
         arrows: false,
         //autoplay: true,
-        autoplaySpeed: 6000,
-        //waitForAnimate: false,
+        //autoplaySpeed: 6000,
         responsive: [
             {
                 breakpoint: 960,
